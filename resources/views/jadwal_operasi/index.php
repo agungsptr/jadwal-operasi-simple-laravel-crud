b12a180e5c5728f7095301f45f53df2768e6a21d
agungsptr <agung.eka1999@gmail.com>
2020-05-10 11:02:30 +0000

    cobain plain php or without

--- resources/views/jadwal_operasi/index.php
+++ resources/views/jadwal_operasi/index.php
@@ -21,16 +21,20 @@
             </tr>
         </thead>
         <tbody>
-            @foreach ($data as $op)
+            <?php 
+                foreach ($data as $op) {
+            ?>
             <tr>
-                <td>{{$op->pasien}}</td>
-                <td>{{$op->dokter}}</td>
-                <td>{{$op->tindakan}}</td>
-                <td>{{$op->status}}</td>
-                <td>{{$op->created_at}}</td>
-                <td>{{$op->jam_keluar}}</td>
+                <td><?php echo($op->pasien) ?></td>
+                <td><?php echo($op->dokter) ?></td>
+                <td><?php echo($op->tindakan) ?></td>
+                <td><?php echo($op->status) ?></td>
+                <td><?php echo($op->created_at) ?></td>
+                <td><?php echo($op->jam_keluar) ?></td>
             </tr>
-            @endforeach
+            <?php
+                }
+            ?>
         </tbody>
     </table>
 </body>
