6ff75433196c8292c1ffed075222a3801210aefe
agungsptr <agung.eka1999@gmail.com>
2020-05-15 15:05:17 +0000

    ganti php

--- resources/views/jadwal_operasi/index.blade.php
+++ resources/views/jadwal_operasi/index.blade.php
@@ -23,10 +23,10 @@
         }
 
         .elip {
-            white-space: nowrap; 
-            width: 200px; 
+            white-space: nowrap;
+            width: 200px;
             overflow: hidden;
-            text-overflow: ellipsis; 
+            text-overflow: ellipsis;
         }
     </style>
 </head>
@@ -45,55 +45,68 @@
                 <!-- start slide -->
                 <div id="slide-ph" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
-                        <?php
-                        for ($i = 0; $i < count($list); $i++) {
-                        ?>
-                            <div class="carousel-item <?php echo ($i == 0) ? 'active' : ''; ?>">
-                                <table class="table table-striped table-bordered">
-                                    <thead class="thead-dark">
-                                        <tr>
-                                            <th style="width: 200px">Nama Pasien</th>
-                                            <th style="width: 170px">Nama Dokter</th>
-                                            <th>Tindakan</th>
-                                            <th>Status</th>
-                                            <th>Jam Masuk</th>
-                                            <th>Jam Selesai</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        foreach ($list[$i] as $op) {
-                                        ?>
-                                            <tr>
-                                                <td><strong><p class="elip">{{strtoupper($op->pasien)}}</p></strong></td>
-                                                <td><p class="elip">{{strtoupper($op->dokter)}}</p></td>
-                                                <td><?php echo strtoupper($op->tindakan) ?></td>
-                                                <td class="text-center"><h5><span class="badge badge-pill badge-primary"><?php echo strtoupper($op->status) ?></span></h5></td>
-                                                <td class="text-danger text-center"><h5><strong>{{substr($op->created_at, 11, 19)}}</strong></h5></td>
-                                                <td class="text-success text-center"><h5><strong>{{substr($op->jam_keluar, 11, 19)}}</strong></h5></td>
-                                            </tr>
-                                        <?php
-                                        }
-                                        ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        <?php
-                        }
-                        ?>
+                        @for ($i = 0; $i < count($list); $i++) <div
+                            class="carousel-item <?php echo ($i == 0) ? 'active' : ''; ?>">
+                            <table class="table table-striped table-bordered">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th style="width: 200px">Nama Pasien</th>
+                                        <th style="width: 170px">Nama Dokter</th>
+                                        <th>Tindakan</th>
+                                        <th>Status</th>
+                                        <th>Jam Masuk</th>
+                                        <th>Jam Selesai</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($list[$i] as $op)
+                                    <tr>
+                                        <td>
+                                            <strong>
+                                                <p class="elip">{{strtoupper($op->pasien)}}</p>
+                                            </strong>
+                                        </td>
+                                        <td>
+                                            <p class="elip">{{strtoupper($op->dokter)}}</p>
+                                        </td>
+                                        <td>
+                                            {{strtoupper($op->tindakan)}}
+                                        </td>
+                                        <td class="text-center">
+                                            <h5>
+                                                <span class="badge badge-pill badge-primary">
+                                                    {{strtoupper($op->status)}}
+                                                </span>
+                                            </h5>
+                                        </td>
+                                        <td class="text-danger text-center">
+                                            <h5><strong>{{substr($op->created_at, 11, 19)}}</strong></h5>
+                                        </td>
+                                        <td class="text-success text-center">
+                                            <h5><strong>{{substr($op->jam_keluar, 11, 19)}}</strong></h5>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
                     </div>
+                    @endfor
                 </div>
-                <!-- end slide -->
             </div>
+            <!-- end slide -->
         </div>
+    </div>
     </div>
 
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
     <script src="/js/jquery.min.js"></script>
     <script>
